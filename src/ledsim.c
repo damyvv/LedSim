8e143826f4a8a53cb627ae9cb3ec60692a9eac56
Damy van Valenberg <damyvv@hotmail.com>
2019-10-31 00:12:07 +0100

    Fixed compile warnings

--- src/ledsim.c
+++ src/ledsim.c
@@ -2,12 +2,13 @@
 
 #include <assert.h>
 #include <math.h>
-#include <string.h>
+#include <stdlib.h>
 
-#include <SDL.h>
+#include <SDL_events.h>
+#include <SDL_video.h>
 #include <SDL_opengl.h>
 
-#ifdef _MSC_BUILD
+#ifdef __WIN32__
 #include <Windows.h>
 #else
 #include <pthread.h>
@@ -19,9 +20,10 @@ static void ledsim_main_thread();
 static void draw(void);
 static void drawCircle(int x, int y, int radius, ledsim_color_t color);
 
-#ifdef _MSC_BUILD
+#ifdef __WIN32__
 DWORD WINAPI ledsim_win_thread(void* data) {
 	ledsim_main_thread();
+	return 0;
 }
 #else
 void* ledsim_unix_thread(void* data) {
@@ -44,7 +46,7 @@ static int ledcount;
 volatile ledsim_color_t* leds;
 
 static inline void ledsim_spawn_thread() {
-#ifdef _MSC_BUILD
+#ifdef __WIN32__
 	HANDLE thread = CreateThread(NULL, 0, ledsim_win_thread, NULL, 0, NULL);
 #else
 	pthread_t thread;
@@ -64,11 +66,11 @@ int ledsim_start(uint32_t led_count, uint32_t r, uint32_t c, ledsim_finish_callb
 	width =  LEDSIM_LED_RADIUS * 2 * cols + ((cols + 1) * LEDSIM_BORDER);
 	height = LEDSIM_LED_RADIUS * 2 * rows + ((cols + 1) * LEDSIM_BORDER);
 
-	size_t bytes = sizeof(ledsim_color_t) * ledcount;
-	leds = malloc(bytes);
-	memset((void*) leds, 0, bytes);
+	leds = (ledsim_color_t*) calloc(ledcount, sizeof(ledsim_color_t));
 
 	ledsim_spawn_thread();
+
+	return 0;
 }
 
 void ledsim_setled(int index, ledsim_color_t color) {
@@ -116,7 +118,7 @@ static void draw(void) {
 		int posX = (i % cols) * (LEDSIM_BORDER + LEDSIM_LED_RADIUS * 2) + LEDSIM_BORDER + LEDSIM_LED_RADIUS;
 		int posY = (i / cols) * (LEDSIM_BORDER + LEDSIM_LED_RADIUS * 2) + LEDSIM_BORDER + LEDSIM_LED_RADIUS;
 		drawCircle(posX, posY, LEDSIM_LED_RADIUS, (ledsim_color_t) { 0xA0, 0xA0, 0xA0 });
-		drawCircle(posX, posY, LEDSIM_LED_RADIUS *0.9f, leds[i]);
+		drawCircle(posX, posY, (int) (LEDSIM_LED_RADIUS * 0.9f), leds[i]);
 	}
 }
 
@@ -125,12 +127,12 @@ static void drawCircle(int x, int y, int radius, ledsim_color_t color) {
 	glColor3f(color.r / 255.0f, color.g / 255.0f, color.b / 255.0f);
 
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(x, y);
+	glVertex2f((GLfloat) x, (GLfloat) y);
 	const static int res = 100;
 	for (int i = 0; i <= res; i++) {
 		glVertex2f(
-			((float) x) + cos(i * 2 * M_PI / res) * ((float) radius),
-			((float) y) + sin(i * 2 * M_PI / res) * ((float) radius)
+			(GLfloat) (((float) x) + cos(i * 2 * M_PI / res) * ((float) radius)),
+			(GLfloat) (((float) y) + sin(i * 2 * M_PI / res) * ((float) radius))
 		);
 	}
 	glEnd();
